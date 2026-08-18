d59e20245185631eed77400129e0e4d71b409664
Santiago Arranz-Olmos <santiago.arranz1@gmail.com>
2026-08-18 13:15:17 +0200

    allocations

--- proofs/compiler/arm_instr_decl_lemmas.v
+++ proofs/compiler/arm_instr_decl_lemmas.v
@@ -33,7 +33,7 @@ Context
   {wsw : WithSubWord}
   {atoI : arch_toIdent}
   {syscall_state : Type}
-  {sc_sem : syscall_sem syscall_state}
+  {ep : EstateParams syscall_state}
   {pT : progT}
   {sCP : semCallParams}.
 
--- proofs/compiler/arm_params_common_proof.v
+++ proofs/compiler/arm_params_common_proof.v
@@ -45,7 +45,7 @@ Section WITH_PARAMS.
 Context
   {atoI  : arch_toIdent}
   {syscall_state : Type}
-  {sc_sem : syscall_sem syscall_state}
+  {ep : EstateParams syscall_state}
   {call_conv : calling_convention}.
 
 #[local] Existing Instance withsubword.
@@ -115,7 +115,7 @@ Lemma align_eval_instr {lp ls ii xname vi y al} {wy : word Uptr} :
   eval_instr lp li ls = ok (next_vm_ls ls vm').
 Proof.
   move=> h1; set vm := _.[ _ <- _].
-  apply (sem_fopn_args_eval_instr (ls:= ls) (s' := with_vm (to_estate ls) vm)).
+  apply: (sem_fopn_args_eval_instr (ls:= ls) (s' := with_vm (to_estate ls) vm)).
   by apply :  align_sem_fopn_args; rewrite h1 /= truncate_word_u.
 Qed.
 
@@ -296,7 +296,7 @@ Section WITH_PARAMS.
 Context
   {atoI  : arch_toIdent}
   {syscall_state : Type}
-  {sc_sem : syscall_sem syscall_state}
+  {ep : EstateParams syscall_state}
   {call_conv : calling_convention}
 .
 
