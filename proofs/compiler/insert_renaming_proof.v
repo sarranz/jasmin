ba51b278ee30311b69eaed2a355b7c53d02ce977
Santiago Arranz-Olmos <santiago.arranz1@gmail.com>
2026-08-20 00:49:56 +0200

    wip

--- proofs/compiler/insert_renaming_proof.v
+++ proofs/compiler/insert_renaming_proof.v
@@ -115,7 +115,9 @@ Section WITH_PARAMS.
     Context
       {E E0: Type → Type}
         {wE: with_Error E E0}
-        {rE: EventRels E0}.
+        {rE: EventRels E0}
+        {rndE : with_RndEvent syscall_state E0}
+        {rndE_refl : RndRels_refl rE}.
 
     Context (insert_renaming_p: fun_info → bool).
     Context (p: prog) (ev: extra_val_t).
@@ -137,7 +139,7 @@ Section WITH_PARAMS.
     Lemma it_insert_renaming_rec (fd: fundef) :
       (∀ ii1 ii2 fn1 fn2, wequiv_f_rec p p' ev ev uincl_spec pre_incl ii1 ii2 fn1 fn2 post_incl) →
       wequiv (rE0 := relEvent_recCall uincl_spec) p p' ev ev (st_uincl tt) (f_body fd) (f_body fd) (st_uincl tt).
-    Proof.
+    Proof using rndE_refl.
       move => hrec.
       apply: (cmd_rect (Pr := Pi_r) (Pi := Pi) (Pc := Pc)).
       - done.
@@ -159,7 +161,7 @@ Section WITH_PARAMS.
 
     Theorem it_insert_renaming_callP fn :
       wiequiv_f p p' ev ev pre_incl fn fn post_incl.
-    Proof.
+    Proof using rndE_refl.
       apply wequiv_fun_ind' => {} fn _ fs ft [] <- hfsu fd hget.
       exists (insert_renaming_fd insert_renaming_p fd).
       - by rewrite get_map_prog hget.
